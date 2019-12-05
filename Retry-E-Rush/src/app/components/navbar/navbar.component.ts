a0b56b1e0befba5f2e659ebd174a1879c920bd27
Carlos Becerra <A01153661@itesm.mx>
2019-12-04 22:15:03 -0600

    Definir opciones que requieren Login

--- Retry-E-Rush/src/app/components/navbar/navbar.component.ts
+++ Retry-E-Rush/src/app/components/navbar/navbar.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
-
+  public app_name: string = 'E-Rush';
+  public isLogged: boolean = false;
   ngOnInit() {
   }
 
