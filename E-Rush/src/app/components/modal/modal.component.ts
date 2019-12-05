d66f5fe894ca11721e847bfa5fc9d4e0a4ded77b
StarNiGHTS <xpkmn64x@gmail.com>
2019-12-04 19:27:35 -0600

    pre-retry

--- E-Rush/src/app/components/modal/modal.component.ts
+++ E-Rush/src/app/components/modal/modal.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataApiService } from "../../services/data-api.service";
+import { GamingInterface } from "../../models/gaming";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"]
 })
 export class ModalComponent implements OnInit {
+  constructor(private dataApi: DataApiService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSaveGaming(gamingForm: NgForm): void {
+    this.dataApi.addGaming(gamingForm);
   }
-
 }
