18ba01fa9beef1428dc2c749713f0033a7af3e7c
Carlos Becerra <A01153661@itesm.mx>
2019-12-06 01:32:31 -0600

    Barra imagen y creando un registro

--- Retry-E-Rush/src/app/components/users/register/register.component.ts
+++ Retry-E-Rush/src/app/components/users/register/register.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -11,8 +14,12 @@ import { AngularFireStorage } from '@angular/fire/storage';
 export class RegisterComponent implements OnInit {
 
   constructor(private router: Router, private authService: AuthService, private storage: AngularFireStorage) { }
+  @ViewChild('imageUser') inputImageUser: ElementRef;
   public email: string = '';
   public password: string = '';
+
+  uploadPercent: Observable<number>;
+  urlImage: Observable<string>;
 
   ngOnInit() {
   }
@@ -20,15 +27,31 @@ export class RegisterComponent implements OnInit {
     //console.log('Subir:', e.target.files[0]);
     const id = Math.random().toString(36).substring(2);
     const file = e.target.files[0];
-    const filePath = 'upload/imagen.png';
+    const filePath = `uploads/profile_${id}`;
     const ref = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file);
-    
+    this.uploadPercent = task.percentageChanges();
+    task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL()))
+    .subscribe();
+
   }
   onAddUser(){
     this.authService.registerUser(this.email, this.password)
     .then((res)=> {
-      this.router.navigate(['admin/list-gaming']);
+      this.authService.isAuth().subscribe( user => {
+        if(user){
+          //console.log('userActual', user);
+          user.updateProfile({
+            displayName: '',
+            photoURL: this.inputImageUser.nativeElement.value
+          }).then( function (){
+            console.log('USER UPDATED!');
+          }).catch( function (error){
+            console.log('Error: ', error);
+          });
+        }
+      })
+      //this.router.navigate(['admin/list-gaming']);
     }).catch(err => console.log('err', err.message));
   }
 
