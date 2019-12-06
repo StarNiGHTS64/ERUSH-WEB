59f543f6fe1f42e4cc826ab1dff4358a243290f2
Carlos Becerra <A01153661@itesm.mx>
2019-12-06 01:48:00 -0600

    models

---
+++ Retry-E-Rush/src/app/models/event.ts
@@ -0,0 +1,9 @@
+export interface EventInterface {
+    id ?: string;
+    name ?: string;
+    date ?: string;
+    city ?: string;
+    address ?: string;
+    description ?: string;
+    image ?: string;
+}
---
+++ Retry-E-Rush/src/app/models/gaming.ts
@@ -0,0 +1,9 @@
+export interface GamingInterface {
+    id ?: string;
+    name ?: string;
+    type ?: string;
+    city ?: string;
+    address ?: string;
+    description ?: string;
+    image ?: string;
+}
